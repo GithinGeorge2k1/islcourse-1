2c3ceca2a7d5b479b5aca3d8b123a7e9d6c2bc65
Kranthi Sedamaki <skranthi4444@gmail.com>
2022-10-19 10:22:34 +0530

    commit 6

--- hubconf.py
+++ hubconf.py
@@ -3,6 +3,7 @@ import numpy as np
 from torchvision import datasets
 from torchvision import transforms
 from torch import nn
+from sklearn import metrics
 
 
 def kali():
@@ -66,61 +67,66 @@ def get_model(train_data_loader=None, n_epochs=10):
 
 class cs19b036CNN(nn.Module):
 
-    def __init__(self, numChannels, classes) -> None:
-        super().__init__()
-        # initialize first set of CONV => RELU => POOL layers
-        self.conv1 = Conv2d(in_channels=numChannels, out_channels=20,
-        kernel_size=(5, 5))
-        self.relu1 = ReLU()
-        self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-        # initialize second set of CONV => RELU => POOL layers
-        self.conv2 = Conv2d(in_channels=20, out_channels=50,
-        kernel_size=(5, 5))
-        self.relu2 = ReLU()
-        self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-        # initialize first (and only) set of FC => RELU layers
-        self.fc1 = Linear(in_features=800, out_features=500)
-        self.relu3 = ReLU()
-        # initialize our softmax classifier
-        self.fc2 = Linear(in_features=500, out_features=classes)
-        self.logSoftmax = LogSoftmax(dim=1)
+    def __init__(self, numChannels, classes, config1) -> None:
 
-    
-  def forward(self, x):
-    # pass the input through our first set of CONV => RELU =>
-    # POOL layers
-    x = self.conv1(x)
-    x = self.relu1(x)
-    x = self.maxpool1(x)
-    # pass the output from the previous layer through the second
-    # set of CONV => RELU => POOL layers
-    x = self.conv2(x)
-    x = self.relu2(x)
-    x = self.maxpool2(x)
-    # flatten the output from the previous layer and pass it
-    # through our only set of FC => RELU layers
-    x = flatten(x, 1)
-    x = self.fc1(x)
-    x = self.relu3(x)
-    # pass the output to our softmax classifier to get our output
-    # predictions
-    x = self.fc2(x)
-    output = self.logSoftmax(x)
-    # return the output predictions
-    return output
+        super().__init__()
+        # config1 = [
+        #     (1, 10, (3, 3), 1, 'same'),
+        #     (10, 3, (5, 5), 1, 'same'),
+        #     (3, 1, (7, 7), 1, 'same')
+        # ]
+        self.config = config1
+
+        self.conv_layers = []
+
+        for conv_layer_config in self.config:
+            in_channels, out_channels, kernel_size, stride, padding = conv_layer_config
+            self.conv_layers.append(nn.Conv2d(
+                in_channels, out_channels, kernel_size, stride, padding))
+            self.conv_layers.append(nn.ReLU())
+            self.conv_layers.append(nn.MaxPool2d(2, 2))
+
+        self.fc1 = nn.Linear(in_features=800, out_features=500)
+        self.relu3 = nn.ReLU()
+        # initialize our softmax classifier
+        self.fc2 = nn.Linear(in_features=500, out_features=classes)
+        self.logSoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        # flatten the output from the previous layer and pass it
+        # through our only set of FC => RELU layers
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        # pass the output to our softmax classifier to get our output
+        # predictions
+        x = self.fc2(x)
+        output = self.logSoftmax(x)
+        # return the output predictions
+        return output
+
 
 def get_model_advanced(train_data_loader=None, n_epochs=10, lr=1e-4, config=None):
-    model = None
+    num_classes = len(train_data_loader.dataset.classes)
+    model = cs19b036CNN(train_data_loader.dataset[0][0].shape[0], num_classes, config)
 
-    # In addition,
-    # Refer to config dict, where learning rate is given,
-    # List of (in_channels, out_channels, kernel_size, stride=1, padding='same')  are specified
-    # Example, config = [(1,10,(3,3),1,'same'), (10,3,(5,5),1,'same'), (3,1,(7,7),1,'same')], it can have any number of elements
-    # You need to create 2d convoution layers as per specification above in each element
-    # You need to add a proper fully connected layer as the last layer
+    for epoch in range(n_epochs):
+        print(f"Epoch {epoch+1}\n-------------------------------")
+        for batch, (X, y) in enumerate(train_data_loader):
+            size = len(train_data_loader.dataset)
 
-    # HINT: You can print sizes of tensors to get an idea of the size of the fc layer required
-    # HINT: Flatten function can also be used if required
+            pred = model(X)
+            loss = nn.CrossEntropyLoss()(pred, y)
+            optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * len(X)
+                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
     print('Returning model... (rollnumber: cs19b036)')
 
     return model
@@ -133,6 +139,9 @@ def test_model(model1=None, test_data_loader=None):
     accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
 
     accs = []
+    precs = []
+    recs = []
+    f1s = []
 
     size = len(test_data_loader.dataset)
     num_batches = len(test_data_loader)
@@ -141,14 +150,26 @@ def test_model(model1=None, test_data_loader=None):
     with torch.no_grad():
         for X, y in test_data_loader:
             pred = model1(X)
-            accs.append(np.sum(np.equal(y, pred)) / len(y))
+            accs.append(metrics.accuracy_score(y, pred.argmax(dim=1)))
+            precs.append(metrics.precision_score(y, pred.argmax(dim=1), average='macro'))
+            recs.append(metrics.recall_score(y, pred.argmax(dim=1), average='macro'))
+            f1s.append(metrics.f1_score(y, pred.argmax(dim=1), average='macro'))
+
+
     test_loss /= num_batches
     correct /= size
     print('Returning metrics... (rollnumber: cs19b036)')
 
     accuracy_val = np.mean(accs)
+    precision_val = np.mean(precs)
+    recall_val = np.mean(recs)
+    f1score_val = np.mean(f1s)
 
     return accuracy_val, precision_val, recall_val, f1score_val
+
+
+def _compute_conv_output_shape(h_w, kernel_size=1, stride=1, pad=0):
+    return (h_w[0] + (2 * pad) - (kernel_size - 1) - 1) // stride + 1, (h_w[1] + (2 * pad) - (kernel_size - 1) - 1) // stride + 1
 
 
 def _load_data():
@@ -168,15 +189,28 @@ def _load_data():
         download=True,
         transform=transforms.ToTensor(),
     )
-    
+
     return training_data, test_data
+
 
 if __name__ == '__main__':
     print('Testing hubconf.py')
     kali()
+
+    config1 = [
+        (1, 10, (3, 3), 1, 'same'),
+        (10, 3, (5, 5), 1, 'same'),
+        (3, 1, (7, 7), 1, 'same')
+    ]
 
     trainset, testset = _load_data()
 
     train_data_loader = torch.utils.data.DataLoader(trainset)
 
     model = get_model(train_data_loader, n_epochs=1)
+
+    test_data_loader = torch.utils.data.DataLoader(testset)
+
+    vals = test_model(model, test_data_loader)
+
+    print(vals)
